8f2b9b2032cbfdd11bf93a37c9f58e300eb36295
yub <yamadayub@gmail.com>
2022-08-12 04:18:59 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ document.getElementById("app").innerHTML = `
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  Hogehoge
 </div>
 `;
